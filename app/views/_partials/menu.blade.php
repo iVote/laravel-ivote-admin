39637ac3e47377b2867001f883a1a52f3fbfb49a
Z Webapp <zwebapp@gmail.com>
2013-09-09 23:52:58 +0800

    Added temporary Dashboard and Menu structure

---
+++ app/views/_partials/menu.blade.php
@@ -0,0 +1,34 @@
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+  <!-- Brand and toggle get grouped for better mobile display -->
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="#">iVote</a>
+  </div>
+
+  <!-- Collect the nav links, forms, and other content for toggling -->
+  <div class="collapse navbar-collapse navbar-ex1-collapse">
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#"><span class="glyphicon glyphicon-search"></span>Dashboard</a></li>
+      <li><a href="#">Election</a></li>
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Maintenance <b class="caret"></b></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Members</a></li>
+          <li><a href="#">Member Groups</a></li>
+          <li><a href="#">Positions</a></li>
+          <li><a href="#">Candidates</a></li>
+        </ul>
+      </li>
+      <li><a href="#">Reports</a></li>
+      <li><a href="#">Accounts</a></li>
+      <li><a href="#">Settings</a></li>
+    </ul>
+
+  </div><!-- /.navbar-collapse -->
+
+</nav>
